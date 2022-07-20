fb84d6c2dd4af73a4047db8be199e29ff559287e
Weyoss <weyoss@protonmail.com>
2022-07-20 12:24:45 +0100

    Sync event listeners with redis-smq v7.0.5 updates

--- src/event-listeners/message-rate/consumer-event-listener.ts
+++ src/event-listeners/message-rate/consumer-event-listener.ts
@@ -1,38 +1,117 @@
 import {
-  TQueueParams,
   IEventListener,
-  IEventProvider,
+  TEventListenerInitArgs,
+  TQueueParams,
 } from 'redis-smq/dist/types';
 import { ConsumerMessageRate } from './consumer/consumer-message-rate';
 import { ConsumerMessageRateWriter } from './consumer/consumer-message-rate-writer';
-import { events } from 'redis-smq';
-import { RedisClient } from 'redis-smq-common';
+import { events, Message } from 'redis-smq';
+import {
+  async,
+  createClientInstance,
+  errors,
+  RedisClient,
+} from 'redis-smq-common';
 import { ICallback } from 'redis-smq-common/dist/types';
+import { EventEmitter } from 'events';
 
 export class ConsumerEventListener implements IEventListener {
-  protected consumerMessageRate: ConsumerMessageRate;
+  protected consumerId: string | null = null;
+  protected consumerMessageRate = new Map<TQueueParams, ConsumerMessageRate>();
+  protected redisClient: RedisClient | null = null;
+  protected eventProvider: EventEmitter | null = null;
 
-  constructor(
-    redisClient: RedisClient,
-    consumerId: string,
+  protected onMessageAcknowledged = (message: Message): void => {
+    this.getMessageRate(
+      message.getRequiredQueue(),
+      String(this.consumerId),
+    ).incrementAcknowledged();
+  };
+
+  protected onMessageDeadLettered = (message: Message): void => {
+    this.getMessageRate(
+      message.getRequiredQueue(),
+      String(this.consumerId),
+    ).incrementDeadLettered();
+  };
+
+  protected getMessageRate(
     queue: TQueueParams,
-    eventProvider: IEventProvider,
-  ) {
-    const writer = new ConsumerMessageRateWriter(
-      redisClient,
-      queue,
-      consumerId,
-    );
-    this.consumerMessageRate = new ConsumerMessageRate(writer);
-    eventProvider.on(events.MESSAGE_ACKNOWLEDGED, () =>
-      this.consumerMessageRate.incrementAcknowledged(),
-    );
-    eventProvider.on(events.MESSAGE_DEAD_LETTERED, () =>
-      this.consumerMessageRate.incrementDeadLettered(),
-    );
+    consumerId: string,
+  ): ConsumerMessageRate {
+    let messageRate = this.consumerMessageRate.get(queue);
+    if (!messageRate) {
+      const redisClient = this.getRedisClient();
+      this.redisClient = redisClient;
+      const writer = new ConsumerMessageRateWriter(
+        redisClient,
+        queue,
+        consumerId,
+      );
+      messageRate = new ConsumerMessageRate(writer);
+      this.consumerMessageRate.set(queue, messageRate);
+    }
+    return messageRate;
+  }
+
+  protected getRedisClient(): RedisClient {
+    if (!this.redisClient)
+      throw new errors.PanicError('Expected a RedisClient instance');
+    return this.redisClient;
+  }
+
+  init(args: TEventListenerInitArgs, cb: ICallback<void>): void {
+    const { instanceId, config, eventProvider } = args;
+    this.consumerId = instanceId;
+    this.eventProvider = eventProvider;
+    createClientInstance(config.redis, (err, redisClient) => {
+      if (err) cb(err);
+      else if (!redisClient) cb(new errors.EmptyCallbackReplyError());
+      else {
+        this.redisClient = redisClient;
+        eventProvider.on(
+          events.MESSAGE_ACKNOWLEDGED,
+          this.onMessageAcknowledged,
+        );
+        eventProvider.on(
+          events.MESSAGE_DEAD_LETTERED,
+          this.onMessageDeadLettered,
+        );
+        cb();
+      }
+    });
   }
 
   quit(cb: ICallback<void>): void {
-    this.consumerMessageRate.quit(cb);
+    this.eventProvider?.removeListener(
+      events.MESSAGE_ACKNOWLEDGED,
+      this.onMessageAcknowledged,
+    );
+    this.eventProvider?.removeListener(
+      events.MESSAGE_DEAD_LETTERED,
+      this.onMessageDeadLettered,
+    );
+    async.waterfall(
+      [
+        (cb: ICallback<void>) => {
+          async.eachOf(
+            [...this.consumerMessageRate.values()],
+            (messageRate, idx, done) => messageRate.quit(done),
+            (err) => {
+              if (err) cb(err);
+              else {
+                this.consumerMessageRate.clear();
+                cb();
+              }
+            },
+          );
+        },
+        (cb: ICallback<void>) => {
+          if (this.redisClient) this.redisClient.halt(cb);
+          else cb();
+        },
+      ],
+      cb,
+    );
   }
 }
--- src/event-listeners/message-rate/producer-event-listener.ts
+++ src/event-listeners/message-rate/producer-event-listener.ts
@@ -1,26 +1,58 @@
-import { IEventListener, IEventProvider } from 'redis-smq/dist/types';
 import { ProducerMessageRate } from './producer/producer-message-rate';
 import { ProducerMessageRateWriter } from './producer/producer-message-rate-writer';
 import { events, Message } from 'redis-smq';
 import { ICallback } from 'redis-smq-common/dist/types';
-import { RedisClient } from 'redis-smq-common';
+import {
+  async,
+  createClientInstance,
+  errors,
+  RedisClient,
+} from 'redis-smq-common';
+import { EventEmitter } from 'events';
+import { IEventListener, TEventListenerInitArgs } from 'redis-smq/dist/types';
 
 export class ProducerEventListener implements IEventListener {
-  protected producerMessageRate: ProducerMessageRate;
+  protected producerMessageRate: ProducerMessageRate | null = null;
+  protected redisClient: RedisClient | null = null;
+  protected eventProvider: EventEmitter | null = null;
 
-  constructor(
-    redisClient: RedisClient,
-    producerId: string,
-    eventProvider: IEventProvider,
-  ) {
-    const writer = new ProducerMessageRateWriter(redisClient);
-    this.producerMessageRate = new ProducerMessageRate(writer);
-    eventProvider.on(events.MESSAGE_PUBLISHED, (message: Message) => {
-      this.producerMessageRate.incrementPublished(message.getRequiredQueue());
+  protected onMessagePublished = (message: Message): void => {
+    this.producerMessageRate?.incrementPublished(message.getRequiredQueue());
+  };
+
+  init(args: TEventListenerInitArgs, cb: ICallback<void>): void {
+    const { config, eventProvider } = args;
+    this.eventProvider = eventProvider;
+    createClientInstance(config.redis, (err, redisClient) => {
+      if (err) cb(err);
+      else if (!redisClient) cb(new errors.EmptyCallbackReplyError());
+      else {
+        this.redisClient = redisClient;
+        const writer = new ProducerMessageRateWriter(redisClient);
+        this.producerMessageRate = new ProducerMessageRate(writer);
+        eventProvider.on(events.MESSAGE_PUBLISHED, this.onMessagePublished);
+        cb();
+      }
     });
   }
 
   quit(cb: ICallback<void>): void {
-    this.producerMessageRate.quit(cb);
+    this.eventProvider?.removeListener(
+      events.MESSAGE_PUBLISHED,
+      this.onMessagePublished,
+    );
+    async.waterfall(
+      [
+        (cb: ICallback<void>) => {
+          if (this.producerMessageRate) this.producerMessageRate.quit(cb);
+          else cb();
+        },
+        (cb: ICallback<void>) => {
+          if (this.redisClient) this.redisClient.halt(cb);
+          else cb();
+        },
+      ],
+      cb,
+    );
   }
 }
