f67428ac126c5681d9ccbda83becf816167aaece
Weyoss <weyoss@protonmail.com>
2022-09-22 10:03:14 +0100

    test: increase code coverage

---
+++ tests/tests/message-dto/test00001.test.ts
@@ -0,0 +1,71 @@
+import { MessageDTO } from '../../../src/common/dto/queues/message.DTO';
+import { ValidationError } from 'class-validator';
+import { validateDTO } from '../../../src/common/validate-dto';
+
+test('Message DTO: queue', async () => {
+  const msg: Record<string, any> = {
+    body: 'some data',
+    scheduledCron: null,
+    scheduledDelay: null,
+    scheduledRepeatPeriod: null,
+    priority: null,
+    queue: {
+      name: 'queue_1663609095187',
+      ns: 'default',
+    },
+    messageState: {
+      publishedAt: 1663609095245,
+      scheduledAt: null,
+      uuid: '86c00b3a-0fbc-48ab-bbaa-ba45ded62d79',
+      scheduledCronFired: false,
+      attempts: 0,
+      scheduledRepeatCount: 0,
+      expired: false,
+      nextScheduledDelay: 0,
+      nextRetryDelay: 0,
+    },
+    exchange: {
+      destinationQueue: {
+        name: 'queue_1663609095187',
+        ns: 'default',
+      },
+      exchangeTag: 'DirectExchange-0dfa0e42-69a0-4146-b8f7-6fe6382ef177',
+      bindingParams: 'queue_1663609095187',
+      type: 0,
+    },
+    createdAt: 1663609095245,
+    ttl: 0,
+    retryThreshold: 3,
+    retryDelay: 60000,
+    consumeTimeout: 0,
+    scheduledRepeat: 0,
+  };
+  await validateDTO(MessageDTO, msg);
+
+  msg.queue = 123;
+  await expect(async () => validateDTO(MessageDTO, msg)).rejects.toBeInstanceOf(
+    ValidationError,
+  );
+
+  msg.queue = true;
+  await expect(async () => validateDTO(MessageDTO, msg)).rejects.toBeInstanceOf(
+    ValidationError,
+  );
+
+  msg.queue = null;
+  await validateDTO(MessageDTO, msg);
+
+  msg.queue = {
+    ns: 'ns1',
+    name: 'queue1',
+  };
+  await validateDTO(MessageDTO, msg);
+
+  msg.queue = {
+    ns: 'ns1',
+    q: 'queue1',
+  };
+  await expect(async () => validateDTO(MessageDTO, msg)).rejects.toBeInstanceOf(
+    ValidationError,
+  );
+});
---
+++ tests/tests/message-dto/test00002.test.ts
@@ -0,0 +1,86 @@
+import { MessageDTO } from '../../../src/common/dto/queues/message.DTO';
+import { validateDTO } from '../../../src/common/validate-dto';
+import { ValidationError } from 'class-validator';
+import { EExchangeType } from 'redis-smq/dist/types';
+
+test('Message DTO: exchange', async () => {
+  const msg: Record<string, any> = {
+    body: 'some data',
+    scheduledCron: null,
+    scheduledDelay: null,
+    scheduledRepeatPeriod: null,
+    priority: null,
+    queue: null,
+    messageState: {
+      publishedAt: 1663609095245,
+      scheduledAt: null,
+      uuid: '86c00b3a-0fbc-48ab-bbaa-ba45ded62d79',
+      scheduledCronFired: false,
+      attempts: 0,
+      scheduledRepeatCount: 0,
+      expired: false,
+      nextScheduledDelay: 0,
+      nextRetryDelay: 0,
+    },
+    exchange: {
+      destinationQueue: {
+        name: 'queue_1663609095187',
+        ns: 'default',
+      },
+      exchangeTag: 'DirectExchange-0dfa0e42-69a0-4146-b8f7-6fe6382ef177',
+      bindingParams: 'queue_1663609095187',
+      type: 0,
+    },
+    createdAt: 1663609095245,
+    ttl: 0,
+    retryThreshold: 3,
+    retryDelay: 60000,
+    consumeTimeout: 0,
+    scheduledRepeat: 0,
+  };
+  await validateDTO(MessageDTO, msg);
+
+  msg.exchange.bindingParams = null;
+  await expect(async () => validateDTO(MessageDTO, msg)).rejects.toBeInstanceOf(
+    ValidationError,
+  );
+
+  msg.exchange = {
+    destinationQueue: {
+      name: 'queue_1663609095187',
+      ns: 'default',
+    },
+    exchangeTag: 'DirectExchange-0dfa0e42-69a0-4146-b8f7-6fe6382ef177',
+    bindingParams: {
+      ns: 'ns1',
+    },
+    type: EExchangeType.TOPIC,
+  };
+  await expect(async () => validateDTO(MessageDTO, msg)).rejects.toBeInstanceOf(
+    ValidationError,
+  );
+
+  msg.exchange.bindingParams.topic = 'topic';
+  await validateDTO(MessageDTO, msg);
+
+  msg.exchange = null;
+  await validateDTO(MessageDTO, msg);
+
+  msg.exchange = {
+    destinationQueue: {
+      name: 'queue_1663609095187',
+      ns: 'default',
+    },
+    exchangeTag: 'DirectExchange-0dfa0e42-69a0-4146-b8f7-6fe6382ef177',
+    bindingParams: {
+      ns: 'ns1',
+    },
+    type: EExchangeType.FANOUT,
+  };
+  await expect(async () => validateDTO(MessageDTO, msg)).rejects.toBeInstanceOf(
+    ValidationError,
+  );
+
+  msg.exchange.bindingParams = 'my_fanout';
+  await validateDTO(MessageDTO, msg);
+});
